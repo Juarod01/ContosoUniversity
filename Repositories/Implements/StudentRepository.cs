3475d4f8e686c5c97a671808cdf7a1ff2cae51e3
David Rodriguez <jdavid.rodriguez.e@gmail.com>
2020-04-25 20:30:26 -0500

    Desde VS

--- Repositories/Implements/StudentRepository.cs
+++ Repositories/Implements/StudentRepository.cs
@@ -24,7 +24,6 @@ namespace ContosoUniversity.Repositories.Implements
                                       select course).ToListAsync();
 
             return listCourses;
-            //prueba
         }
     }    
 }
