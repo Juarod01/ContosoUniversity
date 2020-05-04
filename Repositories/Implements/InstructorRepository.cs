59f979a1c541558f2e8ec5033586393918017035
David Rodriguez <jdavid.rodriguez.e@gmail.com>
2020-05-03 19:46:27 -0500

    Agregando Repositorios y Servicios faltantes

---
+++ Repositories/Implements/InstructorRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Repositories.Implements
+{
+    public class InstructorRepository : GenericRepository<Instructor>, IInstructorRepository
+    {
+        private SchoolContext _schoolContext;
+        public InstructorRepository(SchoolContext SchoolContext) : base(SchoolContext)
+        {
+            _schoolContext = SchoolContext;
+        }
+    }
+}
